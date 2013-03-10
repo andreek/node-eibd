d1074d35f552c725ab7e824dcb6b8e8d07357d1b
Andree Klattenhoff <andree.k@gmail.com>
2013-03-10 23:02:17 +0100

    code cleanup

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-
-//  Copyright (C) 2012>  Andree Klattenhoff
+//  Copyright (C) 2012  Andree Klattenhoff
 //  
 //  This program is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU General Public License as published by
@@ -19,11 +18,11 @@ var events = require('events'),
     sys = require('sys');
 
 function EIBConnection() {
+  var self = this;
+  events.EventEmitter.call(self);
 
-  events.EventEmitter.call(this);
-
-  this.data = new Buffer([]);
-  this.conn = null;
+  self.data = new Buffer([]);
+  self.conn = null;
 
 }
 sys.inherits(EIBConnection, events.EventEmitter);
@@ -32,21 +31,24 @@ sys.inherits(EIBConnection, events.EventEmitter);
  * Opens a connection eibd over TCP/IP
  */
 EIBConnection.prototype.socketRemote = function(opts, callback) {
+  
+  var self = this;
 
   if(!opts.host || !opts.port) {
-    throw new Error('please provide a host and a port as argument!');
-  } else {
+    callback(new Error('please provide a host and a port as argument!'));
+    return;
+  } 
 
-    this.host = opts.host;
-    this.port = opts.port;
+  self.host = opts.host;
+  self.port = opts.port;
 
-    this.socket = net.connect(opts, callback);
-    
-    this.socket.on('error', this.onError);
-    this.socket.on('error', callback);
-    
-    this.socket.on('data', this.onData);
-  }
+  self.socket = net.connect(opts, callback);
+  
+  self.socket.on('error', self.onError);
+  self.socket.on('error', callback);
+  
+  self.socket.on('data', self.onData);
+  
   
 }
 
@@ -54,70 +56,78 @@ EIBConnection.prototype.socketRemote = function(opts, callback) {
  * error handler
  */
 EIBConnection.prototype.onError = function(err) {
-  console.log('ERROR :\n\n'+err+'\n\nClosing Connection');
-  this.end();
+  var self = this;
+  console.error('[ERROR] :\n\n'+err+'\n\nClosing Connection');
+  self.end();
 }
 
 /**
  * close function
  */
 EIBConnection.prototype.end = function() {
-  if(this.socket) this.socket.end();
+  var self = this;
+  if(self.socket) self.socket.end();
 }
 
 /**
  * for testing stdout data
  */
 EIBConnection.prototype.onData = function(data) {
-  this.data += data;
 }
 
 /**
  * parsing group telegram from groupsocket
  */
 EIBConnection.prototype.__parseTelegram = function(data) {
+
+  var self = this;
+
   var len = data[0];
-  if(len >= 8) {
 
-    // command
-    var input = data[data.length-1];
-    var action = input;
-    
-    
-    // 4 + 5 src adr.
-    var src = (data[3]<<8)|data[4];
-    // 6 + 7 dest adr.
-    var dest = (data[5]<<8)|data[6];
-    var val = null;
-
-    // value is not coded with command 
-    // extra field
-    if(len === 9){
-      action = data[data.length-2];
-      input = input.toString(16);
-    }
-    switch(action&0xC0)  {
-      case 0x80:
-        action = 'Write';
-        val = input;
-
-        // decode command from value
-        if(len === 8) val = val-128;
-        break;
-      case 0x40:
-        action = 'Response';
-        val = input;
-
-        // decode command from value
-        if(len === 8) val = val-64;
-        break;
-      case 0x00:
-        action = 'Read';
-        break;
-    }
-    this.emit('data', action, src, dest, val);
-
+  if(len < 8) {
+    return;
   }
+
+  // command
+  var input = data[data.length-1];
+  var action = input;
+  
+  
+  // 4 + 5 src adr.
+  var src = (data[3]<<8)|data[4];
+  // 6 + 7 dest adr.
+  var dest = (data[5]<<8)|data[6];
+  var val = null;
+
+  // value is not coded with command 
+  // extra field
+  if(len === 9){
+    action = data[data.length-2];
+    input = input.toString(16);
+  }
+  
+  switch(action&0xC0)  {
+    case 0x80:
+      action = 'Write';
+      val = input;
+
+      // decode command from value
+      if(len === 8) val = val-128;
+      break;
+    case 0x40:
+      action = 'Response';
+      val = input;
+
+      // decode command from value
+      if(len === 8) val = val-64;
+      break;
+    case 0x00:
+      action = 'Read';
+      break;
+  }
+
+  self.emit('data', action, src, dest, val);
+
 }
 
 /**
@@ -125,8 +135,12 @@ EIBConnection.prototype.__parseTelegram = function(data) {
  */
 EIBConnection.prototype.openGroupSocket = function(writeOnly, callback) {
   
+  var self = this;
   var arr = new Array(5);
- 
+  
+  arr[0] = 0;
+  arr[1] = 38;
+  
   arr[2] = 0;
   arr[3] = 0;
   if(writeOnly != 0) {
@@ -135,14 +149,9 @@ EIBConnection.prototype.openGroupSocket = function(writeOnly, callback) {
     arr[4] = 0x00;
   }
 
-  arr[0] = 0;
-  arr[1] = 38;
+  self.on('data', callback)
 
-  var self = this;
-
-  this.on('data', callback)
-
-  this.socket.on('data', function(data) {
+  self.socket.on('data', function(data) {
     var offset = 1;
     while(offset < data.length) {
      
@@ -167,7 +176,7 @@ EIBConnection.prototype.openGroupSocket = function(writeOnly, callback) {
     
   });
 
-  this.sendRequest(arr);
+  self.sendRequest(arr);
 
 }
 
@@ -175,6 +184,7 @@ EIBConnection.prototype.openGroupSocket = function(writeOnly, callback) {
  * Opens a connection of type T_Group
  */
 EIBConnection.prototype.openTGroup = function(dest, writeOnly, callback) {
+  var self = this;  
   
   // prepare dest
   var arr = new Array(5);
@@ -182,13 +192,14 @@ EIBConnection.prototype.openTGroup = function(dest, writeOnly, callback) {
   arr[1] = 34;
   arr[2] = (dest>>8) & 0xff;
   arr[3] = dest & 0xff;
+  
   if(writeOnly != 0) {
     arr[4] = 0xff;
   } else {
     arr[4] = 0x00;
   }
   
-  this.socket.once('data', function(data) {
+  self.socket.once('data', function(data) {
     
     if(data[1] === 2) {
       if(data[2]<<8 | data[3] == 34) {
@@ -201,7 +212,8 @@ EIBConnection.prototype.openTGroup = function(dest, writeOnly, callback) {
     }
 
   });
-  this.sendRequest(arr);
+
+  self.sendRequest(arr);
 
 }
 
@@ -209,7 +221,7 @@ EIBConnection.prototype.openTGroup = function(dest, writeOnly, callback) {
  * Sends an APDU
  */
 EIBConnection.prototype.sendAPDU = function(data, callback) {
-
+  var self = this;
   var arr = new Array(data.length+2);
 
   arr[0] = 0;
@@ -219,8 +231,8 @@ EIBConnection.prototype.sendAPDU = function(data, callback) {
   if(data.length === 3) {
     arr[4] = data[2];
   }
-  var self = this;
-  this.sendRequest(arr, function() {
+
+  self.sendRequest(arr, function() {
     self.end();
     if(callback) callback();
   });
@@ -232,6 +244,7 @@ EIBConnection.prototype.sendAPDU = function(data, callback) {
  */
 EIBConnection.prototype.sendRequest = function(input, callback) {
   
+  var self = this;
   var data = new Array(input.length+2);
 
   data[0] = (input.length>>8) &0xff
@@ -243,7 +256,7 @@ EIBConnection.prototype.sendRequest = function(input, callback) {
   
   var buf = tools.pack(data);
   
-  this.socket.write(buf, callback);
+  self.socket.write(buf, callback);
 
 }
 
