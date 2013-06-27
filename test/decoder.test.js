7b97b02f763b38ff4a86c3544813d40b1aa25228
Andree Klattenhoff <andree.k@gmail.com>
2013-06-27 22:21:52 +0200

    more types #3

--- test/decoder.test.js
+++ test/decoder.test.js
@@ -1,27 +1,27 @@
 var assert = require('assert'),
-    Encoder = require('../lib/encoder.js');
+    Decoder = require('../lib/decoder.js');
 
 var enc = null;
 
-describe('Encoder', function() {
+describe('Decoder', function() {
 
   before(function(done) {
-    enc = new Encoder();
+    enc = new Decoder();
     done();
   });
   describe('DPT1', function() {
-    it('should encode DPT1 value 1', function(done) {
+    it('should decode DPT1 value 1', function(done) {
       var data = 65;
-      enc.encode(8, data, function(err, type, value) {
+      enc.decode(8, data, function(err, type, value) {
         assert.equal(err, null);
         assert.equal(type, 'DPT1');
         assert.equal(value, 1);
         done();
       })
     }),
-    it('should encode DPT1 value 0', function(done) {
+    it('should decode DPT1 value 0', function(done) {
       var data = 64;
-      enc.encode(8, data, function(err, type, value) {
+      enc.decode(8, data, function(err, type, value) {
         assert.equal(err, null);
         assert.equal(type, 'DPT1');
         assert.equal(value, 0);
@@ -31,10 +31,10 @@ describe('Encoder', function() {
   });
 
   describe('DPT5', function() {
-    it('should encode DPT5 value', function(done) {
+    it('should decode DPT5 value', function(done) {
       var buf = new Buffer(1);
       buf.writeUInt8(150, 0);
-      enc.encode(9, buf, function(err, type, value) {
+      enc.decode(9, buf, function(err, type, value) {
         assert.equal(err, null);
         assert.equal(type, 'DPT5');
         assert.equal(value, 150);
@@ -45,7 +45,7 @@ describe('Encoder', function() {
       var buf = new Buffer(2);
       buf.writeUInt8(150, 0);
       buf.writeUInt8(151, 1);
-      enc.encode(9, buf, function(err, type, value) {
+      enc.decode(9, buf, function(err, type, value) {
         assert.equal(err.message, 'Invalid data len for DPT5');
         done();
       });
@@ -53,11 +53,11 @@ describe('Encoder', function() {
   });
 
   describe('DPT9', function() {
-    it('should encode DPT9 value', function(done) {
+    it('should decode DPT9 value', function(done) {
       var buf = new Buffer(2);
       buf.writeUInt8(0xa1, 0);
       buf.writeUInt8(0x44, 1);
-      enc.encode(10, buf, function(err, type, value) {
+      enc.decode(10, buf, function(err, type, value) {
         assert.equal(err, null);
         assert.equal(type, 'DPT9');
         assert.equal(parseInt(value), -275);
