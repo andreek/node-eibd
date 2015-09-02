4cefb8e67d09a6ca5ae9cb91bfc64b6701064bab
Snowdd1 <snowdd1.theelk@xoxy.net>
2015-09-02 22:40:31 +0200

    Revert "Subscription functions"
    This reverts commit 411b7f8667eedb9212158f45bd58d6ad3df3e9ae.

--- index.js
+++ index.js
@@ -2,12 +2,9 @@ var Connection = require('./lib/connection');
 var Parser = require('./lib/parser');
 var tools = require('./lib/tools');
 var createMessage = require('./lib/creator.js');
-var buscallbacks = require('./lib/buscallbacks.js')
 
 exports.Connection = Connection;
 exports.Parser = Parser;
 exports.createMessage = createMessage;
 exports.str2addr = tools.str2addr;
 exports.addr2str = tools.adr2str;
-exports.startMonitor = buscallbacks.startMonitor;
-exports.registerGA = buscallbacks.registerGA;
