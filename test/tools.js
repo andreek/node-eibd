c6e515d4d6ef82e4589ba4dd91efe54a192a8d74
marcopiraccini <marco.piraccini@gmail.com>
2015-10-02 09:04:53 +0200

    Fixed test describe

--- test/tools.js
+++ test/tools.js
@@ -14,14 +14,14 @@ describe('Tools tests', function() {
       assert.equal(exp, test[1]);
   });
 
-  it('should create convert a positive float m/exp DPT-9  and back', function() {
+  it('should create convert a positive float to a m/exp DPT-9 and back', function() {
       var float1 = 1244.22;
       var test = tools.convertToDPT9(float1);
       var float2 = tools.convertFromDPT9(test[0], test[1]);
       assert.equal(float1, float2);
   });
 
-  it('should create convert a negative float m/exp DPT-9  and back', function() {
+  it('should create convert a negative float to a m/exp DPT-9 and back', function() {
       var float1 = -12442.23;
       var test = tools.convertToDPT9(float1);
       var float2 = tools.convertFromDPT9(test[0], test[1]);
